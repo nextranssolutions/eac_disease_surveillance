70ff0a99f4da0f930ce097453a41bb4140c7d56b
Nextrans Solutions Limited <146913215+nextranssolutions@users.noreply.github.com>
2025-03-10 13:50:22 +0300

    Minor Update
    Minor Update

--- development/surveillance_app/public/views/dev_portal/surveillance-app/src/app/guards/auth.guard.ts
+++ development/surveillance_app/public/views/dev_portal/surveillance-app/src/app/guards/auth.guard.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { AuthService } from '../core-services/auth.service';
 import { CanActivate, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthenticationService } from '../core-services/authentication/authentication.service';
 
 
 @Injectable({
@@ -10,7 +11,7 @@ import { Observable } from 'rxjs';
 })
 export class AuthGuard implements CanActivate {
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthenticationService, private router: Router) {}
   scrollToTop(): void {
     window.scrollTo({
       top: 0,
@@ -18,7 +19,7 @@ export class AuthGuard implements CanActivate {
     });
   }
   canActivate(): boolean {
-    if (this.authService.isLoggedIn) { // Implement isLoggedIn() in your AuthService
+    if (this.authService.isLoggednIn()) { // Implement isLoggedIn() in your AuthService
       
       return true;
     } else {
